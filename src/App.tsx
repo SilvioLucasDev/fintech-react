2a7c77ddcfd197c12e58b7f870fdb95ea1bcce8e
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-04-06 17:42:00 -0300

    feat: use react-router-dom and add Sales component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { DataContextProvider } from './Context/DataContext';
 import Header from './Components/Header';
 import Sidenav from './Components/Sidenav';
-import { DataContextProvider } from './Context/DataContext';
-import Sale from './Pages/Sale';
 import Summary from './Pages/Summary';
+import Sales from './Pages/Sales';
+import Sale from './Pages/Sale';
 import './Style.css';
 
 function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Summary />
-          <Sale />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Summary />} />
+              <Route path="/vendas" element={<Sales />} />
+              <Route path="/vendas/:id" element={<Sale />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Sale.tsx
+++ src/Pages/Sale.tsx
@@ -1,19 +1,7 @@
-import SaleItem from '../Components/SaleItem';
-import { useData } from '../Context/DataContext';
+import React from 'react';
 
 const Sale = () => {
-  const { data } = useData();
-
-  if (data === null) return null;
-  return (
-    <ul>
-      {data.map((sale) => (
-        <li key={sale.id}>
-          <SaleItem sale={sale} />
-        </li>
-      ))}
-    </ul>
-  );
+  return <div>Sale</div>;
 };
 
 export default Sale;
