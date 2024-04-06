41385e4f2748b8eac067bc1bf6e5884a9b6ddc9b
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-04-06 16:08:41 -0300

    refactor: use DataContext in DateRange

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
 import DateInput from './DateInput';
+import { useData } from '../Context/DataContext';
 
 const DateRange = () => {
-  const [inicio, setInicio] = React.useState('');
-  const [final, setFinal] = React.useState('');
+  const { startDate, setStartDate, endDate, setEndDate } = useData();
 
   return (
     <form className="box flex" onSubmit={(e) => e.preventDefault()}>
       <DateInput
+        id="start"
         label="Início"
-        value={inicio}
-        onChange={({ target }) => setInicio(target.value)}
+        value={startDate}
+        onChange={({ target }) => setStartDate(target.value)}
       />
       <DateInput
+        id="end"
         label="Final"
-        value={final}
-        onChange={({ target }) => setFinal(target.value)}
+        value={endDate}
+        onChange={({ target }) => setEndDate(target.value)}
       />
     </form>
   );
