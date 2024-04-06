844c3511c267045c2b9b2085a140fc75af97a4f3
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-04-06 18:08:23 -0300

    feat: add code of Sale component

--- src/Pages/Sale.tsx
+++ src/Pages/Sale.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../Hooks/useFetch';
+import { ISale } from '../Context/DataContext';
+import Loading from '../Components/Loading';
+
+type ISaleWithoutData = Omit<ISale, 'data'>;
 
 const Sale = () => {
-  return <div>Sale</div>;
+  const { id } = useParams();
+
+  const { data, loading } = useFetch<ISaleWithoutData>(
+    `https://data.origamid.dev/vendas/${id}`,
+  );
+  if (loading === true) return <Loading />;
+  if (data === null) return null;
+  return (
+    <div>
+      <div className="box mb">ID: {data.id}</div>
+      <div className="box mb">Nome: {data.nome}</div>
+      <div className="box mb">
+        Preço:{' '}
+        {data.preco.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
+      </div>
+      <div className="box mb">Status: {data.status}</div>
+      <div className="box mb">Pagamento: {data.pagamento}</div>
+    </div>
+  );
 };
 
 export default Sale;
