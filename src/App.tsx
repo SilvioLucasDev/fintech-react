c84a4cb9884373a663f2f7cafd3e9d62c987f26b
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-04-06 12:13:02 -0300

    feat: add common files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Header from './Components/Header';
+import Sidenav from './Components/Sidenav';
+import Resumo from './Pages/Resumo';
 import './Style.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Sidenav />
+        <Resumo />
+      </main>
+    </div>
+  );
 }
 
 export default App;
