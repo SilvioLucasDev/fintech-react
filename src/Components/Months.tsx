66f2789e09d60dc5109129831fe6808b6a02d33f
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-04-06 16:09:07 -0300

    feat: add Months

---
+++ src/Components/Months.tsx
@@ -0,0 +1,14 @@
+import MonthButton from './MonthButton';
+
+const Months = () => {
+  return (
+    <div className="flex">
+      <MonthButton month={-3} />
+      <MonthButton month={-2} />
+      <MonthButton month={-1} />
+      <MonthButton month={-0} />
+    </div>
+  );
+};
+
+export default Months;
